246f9b56e868c2075a614fa9283ad3ab4100c92b
quynhctzz <145992271+quynhctzz@users.noreply.github.com>
2023-10-10 15:50:23 +0700

    commit

--- baiTap.java
+++ baiTap.java
@@ -1,13 +1,12 @@
-class GFG {
+class lesson1 {
     static void reverse(char[] str, int start, int end)
     {
-      // Temporary variable
-      // to store character
+      
       char temp;
    
       while (start <= end) {
-        // Swapping the first
-        // and last character
+        
+     
         temp = str[start];
         str[start] = str[end];
         str[end] = temp;
@@ -20,15 +19,12 @@ class GFG {
     {
       int word_begin = -1;
    
-      //  /* temp is for word boundary */
+     
       int i = 0;
    
-      /*STEP 1 of the above algorithm */
+     
       while (i < s.length) {
-   
-        /*This condition is to make sure that the
-                string start with valid character (not
-                space) only*/
+                     
         if ((word_begin == -1) && (s[i] != ' ')) {
           word_begin = i;
         }
@@ -39,19 +35,19 @@ class GFG {
           word_begin = -1;
         }
         i++;
-      } /* End of while */
+      } 
    
-      /*STEP 2 of the above algorithm */
+      
       reverse(s, 0, (s.length - 1));
       return s;
     }
    
-    // Driver Code
+    
     public static void main(String[] args)
     {
       String s = "i like this program very much";
    
-      // Function call
+      
       char[] p = reverseWords(s.toCharArray());
       System.out.println(p);
     }
--- baitap2.java
+++ baitap2.java
@@ -1,36 +1,42 @@
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collector;
+
 
 public class baitap2 {
+    public static void main(String[] args){
+
+        List<Integer> unsortedList = Arrays.asList(1,10,9,21,-5,6,-2);
+
+        List<Integer1> sortedList = unsortedList
+             .stream()
+             .sorted()
+             .collect(collectors.toList());
+
+             System.out.println("Before sorting : " + unsortedList);
+             System.out.println("After sorting : " + sortedList);
+
+        List<Integer1> sortedList1 = unsortedList
+             .stream()
+             .sorted(Comparator.reverseOrder())
+             .collect(collectors.toList());
+             
+             System.out.println("Before sorting : " + unsortedList);
+             System.out.println("After sorting : " + sortedList);
+
+
     
-    public static void main(String[] args) {
-        int i;
-        int array[] = { 5, 3, -15, 4, 2, -1, -5, 10 };
-        System.out.println("giam dan");
-        for (i = 0; i < array.length; i++)
-
-
-
-            System.out.println(array[i]);
-
-
-        Arrays.sort(array);
-        System.out.println("tang dan");
-        for (i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-
-        }
-
-
-
+    
+    
+    
+    
     }
 
-
-
-
-
-
-
-
-
 }
+    
+  
+       
+        
 
