9cde3722aefd86f88c2af71c380d4328f32e3495
quynhctzz <145992271+quynhctzz@users.noreply.github.com>
2023-10-10 16:04:43 +0700

    demo commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.*;     
+public class Main
+{
+  public static void main(String[] args) {
+    int a[]={3,6,2,7,1,9,8,0};    
+    Arrays.sort(a);        
+    for(int i=0;i<a.length;i++)        
+    {
+        System.out.print(a[i]+" ");
+    }
+    System.out.println();   
+      for(int i=a.length-1;i>=0;i--)     
+    {
+        System.out.print(a[i]+" ");
+    }        
+  
+  }
+}
